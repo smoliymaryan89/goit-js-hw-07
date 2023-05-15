d440201c8345a4396fe76414ced7d135d38938d6
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-05-15 14:10:13 +0300

    Create 02-lightbox.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const ulEl = document.querySelector('.gallery');
+
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+ulEl.innerHTML = createListItemsMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
